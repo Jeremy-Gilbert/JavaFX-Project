2aadd68b2f6e8d4eba309bd45e1a2b3e100cf609
Jeremy Gilbert <85994395+Jeremy-Gilbert@users.noreply.github.com>
2022-12-20 23:08:45 +0530

    Delete JavaFX-Project directory

--- JavaFX-Project/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.javafxproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.javafxproject to javafx.fxml;
-    exports com.example.javafxproject;
-}
