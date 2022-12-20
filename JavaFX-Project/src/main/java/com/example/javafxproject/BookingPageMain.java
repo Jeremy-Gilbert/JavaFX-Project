5984d5d75442bb70537f11158d6aaee38388fa60
Jeremy Gilbert <85994395+Jeremy-Gilbert@users.noreply.github.com>
2022-12-20 16:44:08 +0530

    Add files via upload

--- JavaFX-Project/src/main/java/com/example/javafxproject/BookingPageMain.java
+++ JavaFX-Project/src/main/java/com/example/javafxproject/BookingPageMain.java
@@ -11,8 +11,8 @@ public class BookingPageMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BookingPageMain.class.getResource("booking-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Booking Service");
         stage.setScene(scene);
         stage.show();
     }
